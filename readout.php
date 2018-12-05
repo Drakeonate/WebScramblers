e504b362dd4f7dbb2b5eed5473f6c5f418208d68
KwesiEssel <44833655+KwesiEssel@users.noreply.github.com>
2018-12-06 00:46:13 +0100

    hmm
    ich pack es mla rein auch wenn ich nicht weiss ob es funktioniert

--- readout.php
+++ readout.php
@@ -2,9 +2,7 @@
 require_once ('konfiguration.php');
 $db_link = mysqli_connect (
                      MYSQL_HOST, 
-                     MYSQL_BENUTZER, 
-                     MYSQL_KENNWORT, 
-                     MYSQL_DATENBANK
+                     MYSQL_Profil, 
                     );
  
 $sql = "SELECT * FROM adressen";
